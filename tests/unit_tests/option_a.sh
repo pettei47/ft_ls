72c1b64922de66ddafafb7c130049c7603d4cf20
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-06-30 10:52:04 +0900

    add test option_a

--- tests/unit_tests/option_a.sh
+++ tests/unit_tests/option_a.sh
@@ -7,11 +7,11 @@ EXPECTS_DIR="../expects"
 LOGS_DIR="../logs"
 
 cd ${TEST_DATA_DIR}
-../../ft_ls > ${OUTPUTS_DIR}/no_args_output
-diff ${OUTPUTS_DIR}/no_args_output ${EXPECTS_DIR}/no_args > ${LOGS_DIR}/no_args.log
+../../ft_ls -a > ${OUTPUTS_DIR}/option_a_output
+diff ${OUTPUTS_DIR}/option_a_output ${EXPECTS_DIR}/option_a > ${LOGS_DIR}/option_a.log
 if [ $? -eq 0 ]; then
   echo -n "[PASS]"
 else
   echo -n "[FAIL]"
 fi
-echo ": test 'no_args'"
+echo ": test 'option_a'"
--- tests/unit_tests/run_all_tests.sh
+++ tests/unit_tests/run_all_tests.sh
@@ -3,3 +3,4 @@
 TESTS_DIR="tests/unit_tests"
 
 bash ${TESTS_DIR}/no_args.sh
+bash ${TESTS_DIR}/option_a.sh
