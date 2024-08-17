8d498ea464db8f67892887eb6e9cf65224ce9c5d
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-08-17 19:22:10 +0900

    refactor exec_ls

--- src/exec_ls.c
+++ src/exec_ls.c
@@ -23,22 +23,45 @@ static File  *get_files_head(Args *args, char *path, DIR *dp, int exit_code) {
   return head;
 }
 
+static void  print_dir_name(char *path, bool print_path, bool endline, int exit_code) {
+  if (!exit_code && print_path) {
+    if (endline) {
+      ft_putendl_fd("", 1);
+    }
+    ft_putstr_fd(path, 1);
+    ft_putendl_fd(":", 1);
+  }
+}
+
+static void  print_error_message(char *path, int exit_code) {
+  if (exit_code) {
+    ft_putstr_fd("ls: cannot open directory '", 2);
+    ft_putstr_fd(path, 2);
+    ft_putstr_fd("': ", 2);
+    ft_putendl_fd(strerror(errno), 2);
+  }
+}
+
+static bool  is_skip_recursive(FileInfo *info, Args *args) {
+  bool is_dir = info->file_mode == S_IFDIR;
+  bool is_current = ft_strcmp(info->path_name, ".") == 0;
+  bool is_parent = ft_strcmp(info->path_name, "..") == 0;
+  bool is_hidden = !args->show_hidden && info->path_name[0] == '.';
+  return !is_dir || is_current || is_parent || is_hidden;
+}
+
 int exec_ls(char *path, Args *args, bool print_path, bool endline) {
   int exit_code = 0;
   errno = 0;
   DIR *dp = opendir(path);
-  if (errno != 0) {
-    exit_code = 1;
-  }
+  if (errno != 0) exit_code = 1;
   File *head = get_files_head(args, path, dp, exit_code);
   if (dp != NULL) closedir(dp);
   if (!head) {
     perror("malloc failed");
     return 42;
   }
-  if (errno != 0) {
-    exit_code = 1;
-  }
+  if (errno != 0) exit_code = 1;
   int files_len = 0;
   File *f = head->next;
   while (f) {
@@ -51,42 +74,21 @@ int exec_ls(char *path, Args *args, bool print_path, bool endline) {
     perror("malloc failed");
     return 42;
   }
-  FileInfo **sorted_infos = sort_infos(infos, files_len, args->order_by_modified_time, args->reverse);
+  FileInfo **sorted_infos = sort_infos(
+    infos, files_len, args->order_by_modified_time, args->reverse
+  );
   if (!sorted_infos) {
     free_file_infos(infos);
     perror("malloc failed");
     return 42;
   }
-  if (!exit_code && print_path) {
-    if (endline) {
-      ft_putendl_fd("", 1);
-    }
-    ft_putstr_fd(path, 1);
-    ft_putendl_fd(":", 1);
-  }
-  if (exit_code) {
-    ft_putstr_fd("ls: cannot open directory '", 2);
-    ft_putstr_fd(path, 2);
-    ft_putstr_fd("': ", 2);
-    ft_putendl_fd(strerror(errno), 2);
-  }
+  print_dir_name(path, print_path, endline, exit_code);
+  print_error_message(path, exit_code);
   print_file_info(sorted_infos, args, true);
   if (args->recursive) {
     for (int i = 0; sorted_infos[i]; i++) {
-      // directoryかどうかを判定
-      if (sorted_infos[i]->file_mode != S_IFDIR) {
-        continue;
-      }
-      if (ft_strcmp(sorted_infos[i]->path_name, ".") == 0 ||
-          ft_strcmp(sorted_infos[i]->path_name, "..") == 0 ) {
-        continue;
-      }
-      if (!args->show_hidden && sorted_infos[i]->path_name[0] == '.') {
-        continue;
-      }
-      if (exec_ls(sorted_infos[i]->stat_path, args, true, true)) {
-        exit_code = 1;
-      }
+      if (is_skip_recursive(sorted_infos[i], args)) continue;
+      exit_code |= exec_ls(sorted_infos[i]->stat_path, args, true, true);
     }
   }
   free_file_infos(sorted_infos);
