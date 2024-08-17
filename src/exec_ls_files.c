db16c29ac10b3e34ccacf69d07f72c22dc25c311
pettei47 <kitagawa.teppei.33c@gmail.com>
2024-08-17 18:43:34 +0900

    refactor exec_ls_files

--- src/exec_ls_files.c
+++ src/exec_ls_files.c
@@ -1,10 +1,9 @@
 #include "ft_ls.h"
 
-int exec_ls_files(char **files, Args *args) {
+static File  *get_files_head(char **files, Args *args) {
   File *head = (File *)malloc(sizeof(File));
   if (!head) {
-    perror("malloc failed");
-    return 42;
+    return NULL;
   }
   head->path_name = NULL;
   head->stat = NULL;
@@ -15,41 +14,37 @@ int exec_ls_files(char **files, Args *args) {
     File *f = get_file_stat(args, files[i], NULL, NULL);
     if (!f) {
       free_files(head);
-      perror("malloc failed");
-      return 42;
+      return NULL;
     }
     current->next = f;
     current = f;
   }
+  return head;
+}
 
-  // stats to file info
+int exec_ls_files(char **files, Args *args) {
+  File *head = get_files_head(files, args);
   int files_len = 0;
   File *f = head->next;
   while (f) {
     ++files_len;
     f = f->next;
   }
-
   FileInfo **infos = get_file_infos(head, files_len);
   free_files(head);
   if (!infos) {
     perror("malloc failed");
     return 42;
   }
-
-  // sort infos
-  FileInfo **sorted_infos = sort_infos(infos, files_len, args->order_by_modified_time, args->reverse);
+  FileInfo **sorted_infos = sort_infos(
+    infos, files_len, args->order_by_modified_time, args->reverse
+  );
   if (!sorted_infos) {
     free_file_infos(infos);
     perror("malloc failed");
     return 42;
   }
-
-  // 出力する
   print_file_info(sorted_infos, args, false);
-
-  // free
   free_file_infos(sorted_infos);
-
   return 0;
 }
